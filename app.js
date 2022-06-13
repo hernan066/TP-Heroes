57b0f376dfab82aa7481f9e15a26ef3a8034c9e0
hernan0066 <hernan0066@gmail.com>
2022-06-13 17:53:24 -0300

    Vistas agragadas

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
 //iniciar servidor express
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = 3030;
+
+
+
+//rutas
+//app.get('/', (req, res) => {res.send('Hello World');});
+//app.get('/', (req, res) => {res.sendFile(__dirname + '/views/home.html');});
+app.get('/', (req, res) => {res.sendFile( path.join(__dirname, 'views', 'home.html'));});
+app.get('/login', (req, res) => {res.sendFile( path.join(__dirname, 'views', 'login.html'));});
+app.get('/register', (req, res) => {res.sendFile( path.join(__dirname, 'views', 'register.html'));});
+
+
+
+
 
 
 //iniciar servidor
@@ -9,10 +23,6 @@ app.listen(port, () => {
     console.log( `Servidor iniciado en puerto ${port}` );
 });
 
-//rutas
-app.get('/', (req, res) => {
-    res.send('Hello World');
-});
 
 
 

21f5d6d0a06811f7391ff4bbfe6ea6df3a75794e
hernan0066 <hernan0066@gmail.com>
2022-06-14 17:39:28 -0300

    Borrando comentarios extras

--- app.js
+++ app.js
@@ -1,14 +1,3 @@
-/* ● / -> Debe direccionar al recurso index.html.
-● /babbage -> Debe direccionar al recurso babbage.html
-● /berners-lee -> Debe direccionar al recurso berners-lee.html.
-● /clarke -> Debe direccionar al recurso clarke.html.
-● /hamilton -> Debe direccionar al recurso hamilton.html.
-● /hopper -> Debe direccionar al recurso hopper.html.
-● /lovelace -> Debe direccionar al recurso lovelace.html.
-● /turing-> Debe direccionar al recurso turing.html.
-*/
-
-//iniciar servidor express
 const express = require('express');
 const path = require('path');
 const app = express();

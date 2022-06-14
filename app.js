a2c721cf0843ef1e32ed211f75bbc4764c3b1a8a
hernan066 <hernan0066@gmail.com>
2022-06-14 00:02:03 -0300

    TP Heroes

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
+/* ● / -> Debe direccionar al recurso index.html.
+● /babbage -> Debe direccionar al recurso babbage.html
+● /berners-lee -> Debe direccionar al recurso berners-lee.html.
+● /clarke -> Debe direccionar al recurso clarke.html.
+● /hamilton -> Debe direccionar al recurso hamilton.html.
+● /hopper -> Debe direccionar al recurso hopper.html.
+● /lovelace -> Debe direccionar al recurso lovelace.html.
+● /turing-> Debe direccionar al recurso turing.html.
+*/
+
 //iniciar servidor express
 const express = require('express');
 const path = require('path');
@@ -7,12 +17,18 @@ const port = 3030;
 
 
 //rutas
-//app.get('/', (req, res) => {res.send('Hello World');});
-//app.get('/', (req, res) => {res.sendFile(__dirname + '/views/home.html');});
-app.get('/', (req, res) => {res.sendFile( path.join(__dirname, 'views', 'home.html'));});
-app.get('/login', (req, res) => {res.sendFile( path.join(__dirname, 'views', 'login.html'));});
-app.get('/register', (req, res) => {res.sendFile( path.join(__dirname, 'views', 'register.html'));});
 
+
+app.get('/', (req, res) => {res.sendFile( path.join(__dirname, 'views', 'index.html'));});
+app.get('/babbage', (req, res) => {res.sendFile( path.join(__dirname, 'views', 'babbage.html'));});
+app.get('/berners-lee', (req, res) => {res.sendFile( path.join(__dirname, 'views', 'berners-lee.html'));});
+app.get('/clarke', (req, res) => {res.sendFile( path.join(__dirname, 'views', 'clarke.html'));});
+app.get('/hamilton', (req, res) => {res.sendFile( path.join(__dirname, 'views', 'hamilton.html'));});
+app.get('/hopper', (req, res) => {res.sendFile( path.join(__dirname, 'views', 'hopper.html'));});
+app.get('/lovelace', (req, res) => {res.sendFile( path.join(__dirname, 'views', 'lovelace.html'));});
+app.get('/turing', (req, res) => {res.sendFile( path.join(__dirname, 'views', 'turing.html'));});
+
+app.use(express.static('public'));
 
 
 
